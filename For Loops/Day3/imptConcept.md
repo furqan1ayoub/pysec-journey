6b90b447f3a9600b2c7a228f2ed948bfcec3f799
furqanAyoub <furqanayoubmir@gmail.com>
2025-01-11 19:44:06 +0530

    Day4Simple-BruteForce

---
+++ For Loops/Day3/imptConcept.md
@@ -0,0 +1,6 @@
+```python
+#we use nested loops when we have to -> do sth with the each iterated value like :- prime no's 
+#in it we get all no's to 2 - 101 as 1 and itself isn't considered(as prime no's should be divisible by 1 and itself)
+#so we iterate from 2 -99 and on each
+#value we - checked through for loop is it divisble by the nos upto it i.e 2 to it and this goes on to 99 
+```
---
+++ For Loops/Day4/new.py
@@ -0,0 +1,26 @@
+import time
+
+# Define usernames and passwords
+usernames = ["admin", "user", "guest", "john_doe", "jane_smith", "support", "root", "superuser", "manager", "employee"]
+passwords = ["1234", "password", "admin", "secret123", "qwerty", "helpdesk", "toor", "strongpass", "secure123", "mypassword"]
+
+# Simulate brute-force attack
+for username in usernames:
+    logged_in = False  # Flag to track successful login
+
+    for password in passwords:
+        print(f"Trying username: {username}, password: {password}")
+        time.sleep(0.1)  # Simulate delay
+
+        if username == "root" and password == "secure123":
+            print("Successful Login!")
+            print("------------------------")
+            print(f"Username: {username}")
+            print(f"Password: {password}")
+            logged_in = True
+            break  # Break out of the password loop
+
+    if logged_in:
+        break  # Break out of the username loop
+
+print("Brute-force attack complete !")
